b01b76382b4feeea9d8535781a9ac5e9f32c9994
lymisscarol <liuye2014@hotmail.com>
2015-10-13 15:26:23 -0400

    Add more functions to AndroidApp

--- Challenge3/app/src/main/java/com/example/liuye/challenge3/MainActivity.java
+++ Challenge3/app/src/main/java/com/example/liuye/challenge3/MainActivity.java
@@ -8,8 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -38,25 +43,90 @@ public class MainActivity extends AppCompatActivity {
 
 
         try {
-            HttpRequest httpRequest = new HttpRequest();
+            final Button togglePIN = (Button) findViewById(R.id.toggle_pin);
+            //togglePIN.setVisibility(View.INVISIBLE);
+
+            final TextView txtPIN = (TextView) findViewById(R.id.pin);
+            txtPIN.setVisibility(View.INVISIBLE);
+
+            final Spinner pinSpinner = (Spinner) findViewById(R.id.pin_Id);
+            pinSpinner.setVisibility(View.INVISIBLE);
+
+            final RadioGroup statusGroup = (RadioGroup) findViewById(R.id.radio_status);
+            statusGroup.setVisibility(View.INVISIBLE);
+
+            final HttpRequest httpRequest = new HttpRequest();
             //String s = httpRequest.sendGet("ec544group2.ddns.net" + PORT, "device");
             JSONObject jsonObject = httpRequest.sendGet("ec544group2.ddns.net" + PORT, "device");
 
             JSONArray jsonArray = jsonObject.getJSONArray("deviceID");
-            ArrayList<String> devices = getDeviceList(jsonArray);
-            Spinner spinner = (Spinner) findViewById(R.id.device_Id);
+            ArrayList<String> devices = getList(jsonArray);
+            final Spinner deviceSpinner = (Spinner) findViewById(R.id.device_Id);
+            deviceSpinner.setVisibility(View.INVISIBLE);
 
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, devices);
-            arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            ArrayAdapter<String> deviceArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, devices);
+            deviceArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-            spinner.setAdapter(arrayAdapter);
+            deviceSpinner.setAdapter(deviceArrayAdapter);
+
+            deviceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    try {
+                        txtPIN.setVisibility(View.VISIBLE);
+                        pinSpinner.setVisibility(View.VISIBLE);
+                        togglePIN.setVisibility(View.VISIBLE);
+                        statusGroup.setVisibility(View.VISIBLE);
+
+                        final String deviceId = (String) deviceSpinner.getSelectedItem();
+                        JSONObject pinJSONObject = httpRequest.sendGet("ec544group2.ddns.net" + PORT, "device" + deviceId);
+
+                        JSONArray pinJSONArray = pinJSONObject.getJSONArray("PINId");
+                        ArrayList<String> pins = getList(pinJSONArray);
+
+
+                        ArrayAdapter<String> pinArrayAdapter = getAdapter(pins);
+                        pinArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                        pinSpinner.setAdapter(pinArrayAdapter);
+
+                        togglePIN.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                String pinId = (String) pinSpinner.getSelectedItem();
+
+                                RadioButton radioLow = (RadioButton) findViewById(R.id.radio_low);
+                                RadioButton radioHigh = (RadioButton) findViewById(R.id.radio_high);
+
+                                String status = "";
+                                if(radioLow.isChecked())
+                                    status = "LOW";
+                                if(radioHigh.isChecked())
+                                    status = "HIGH";
+
+                                httpRequest.sendPost("ec544group2.ddns.net" + PORT, "device?" + deviceId + "/pin/" + pinId + "/status/" + status);
+                            }
+                        });
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
         }
         catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    private ArrayList<String> getDeviceList(JSONArray jsonArray) {
+    private ArrayList<String> getList(JSONArray jsonArray) {
         ArrayList<String> list = new ArrayList<String>();
 
         try{
@@ -68,6 +138,11 @@ public class MainActivity extends AppCompatActivity {
             ex.printStackTrace();
         }
         return list;
+    }
+
+    private ArrayAdapter<String> getAdapter(ArrayList<String> arrayList) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, arrayList);
+        return adapter;
     }
 
     @Override
@@ -91,4 +166,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
